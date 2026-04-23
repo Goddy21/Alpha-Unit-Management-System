542895ac797bd3b7711ca88d74f07bd80498de1c
Goddy21 <godblessodhiambo@gmail.com>
2026-04-23 23:28:26 +0300

    ssl

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -8,6 +8,7 @@ const pool = new Pool({
   database: process.env.DB_NAME || 'isms_db',
   user: process.env.DB_USER || 'postgres',
   password: process.env.DB_PASSWORD,
+  ssl: { rejectUnauthorized: false },
   max: 20, // Maximum number of clients in the pool
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
--- Backend/src/server.js
+++ Backend/src/server.js
@@ -7,6 +7,7 @@ require('dotenv').config();
 
 
 const app = express();
+app.set('trust proxy', 1);
 
 
 // ==================== MIDDLEWARE ====================
